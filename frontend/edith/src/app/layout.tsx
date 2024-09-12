c558454ff242dd25d3010956080d784b2295d24e
Ritwikgotbugs <shritwik04@gmail.com>
2024-09-12 20:53:06 +0530

    Adds Analytics for vercel

--- frontend/edith/src/app/layout.tsx
+++ frontend/edith/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
+      <Analytics />
       <Toaster />
       </body>
      
